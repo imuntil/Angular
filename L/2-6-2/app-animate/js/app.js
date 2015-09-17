f2cd0d4f91ba416d62fa36a5fdce2f5567d3dc1c
imuntil <imuntil@qq.com>
2015-09-17 23:33:26 +0800

    home.9-17

--- L/2-6-2/app-animate/js/app.js
+++ L/2-6-2/app-animate/js/app.js
@@ -2,14 +2,14 @@
  * Created by jtun02 on 15/8/18.
  */
 var bookStoreApp = angular.module('bookStoreApp', [
-    'ngRoute', 'ngAnimate','bookStoreCtrls'
+    'ngRoute', 'ngAnimate','bookStoreCtrls', 'app.interceptor'
 ])
     .run(['$rootScope', '$location', function ($rootScope, $location) {
         $rootScope.$on('$routeChangeStart', function (evt, next, current) {
 //                console.log('$routeChangeStart-next:'+next);
 //                console.log('$routeChangeStart-current:'+current);
-                console.log(next);
-                console.log(current);
+//                console.log(next);
+//                console.log(current);
             });
         $rootScope.$on('$routeChangeSuccess', function (evt, current, previous) {
 //                console.log('$routeChangeSuccess-current:'+current);
@@ -30,4 +30,25 @@ bookStoreApp.config(function($routeProvider) {
         .otherwise({
             redirectTo:'/hello'
         });
+}).config(function ($httpProvider) {
+    $httpProvider.interceptors.push('interceptor');
 });
+
+angular.module('app.interceptor', [])
+    .factory('interceptor', function ($q, $rootScope) {
+        var interceptor = {
+            response: function (response) {
+                console.log('success');
+                console.log(response.config.url);
+                $rootScope.$broadcast('got:success');
+                return response;
+            },
+            responseError: function (rejection) {
+                console.log('error:' + rejection.status);
+                console.log(rejection.config.url);
+                $rootScope.$broadcast('got:failure');
+                return $q.reject(rejection);
+            }
+        };
+        return interceptor;
+    });
