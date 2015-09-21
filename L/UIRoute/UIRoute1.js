07da9f674b54409d273637048e4d9dff5846a85b
imuntil <imuntil@qq.com>
2015-09-21 18:55:52 +0800

    route-resolve

--- L/UIRoute/UIRoute1.js
+++ L/UIRoute/UIRoute1.js
@@ -14,10 +14,28 @@ myUIRoute.config(function($stateProvider, $urlRouterProvider) {
             templateUrl:'tpls/state2.html'
         })
         .state('state1.list', {
+            resolve:{
+                example: function () {
+                    return [
+                        'a',
+                        'example',
+                        'of',
+                        'resolve'
+                    ]
+                },
+                httpService: function ($http) {
+                    return $http({
+                        method:'GET',
+                        url:'../service/testJson.json'
+                    });
+                }
+            },
             url:'/list',
             templateUrl:'tpls/state1.list.html',
-            controller: function($scope) {
-                $scope.items = ['A', 'List', 'Of', 'Items'];
+            controller: function($scope, example, httpService) {
+                //$scope.items = ['A', 'List', 'Of', 'Items'];
+                //$scope.items = example;
+                $scope.items = httpService.data;
             }
         })
         .state('state2.list', {
