92e09f50f2169a3f4ec5c25f425bdeb9fd282042
imuntil <imuntil@qq.com>
2015-12-13 01:58:57 +0800

    angular-shuuemura

---
+++ P/shuuemura/main.js
@@ -0,0 +1,48 @@
+/**
+ * Created by 斌 on 2015/12/13.
+ */
+(function () {
+    angular.module('Shuuemura', [
+        'ngAnimate',
+        'angular-gestures'
+    ])
+        .controller('PagesController', PagesController)
+        .config(config);
+
+    function PagesController() {
+        var vm = this;
+        vm.paging = 1;
+        vm.DD = false;      //direction down
+        vm.swipeDown = swipeDown;
+        vm.swipeUp = swipeUp;
+
+        function swipeUp(e) {
+            console.log(e);
+            vm.DD = false;
+            if (vm.paging >= 7) {
+                vm.paging = 7;
+            } else {
+                vm.paging += 1;
+            }
+        }
+        function swipeDown(e) {
+            console.log(e);
+            vm.DD = true;
+            if (vm.paging <= 1) {
+                vm.paging = 1;
+            } else {
+                vm.paging -= 1;
+            }
+        }
+    }
+
+    config.$inject = ['hammerDefaultOptsProvider'];
+    function config(hammerDefaultOptsProvider) {
+
+        hammerDefaultOptsProvider.set({
+            recognizers: [
+                [Hammer.Swipe, {}]
+            ]
+        });
+    }
+})();
